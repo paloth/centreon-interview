64588df905383cb403d64dc64008cc03ae49f741
Paloth <lth.paul@hotmail.fr>
2020-12-14 20:08:38 +0100

    build: add env variables

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,22 +5,24 @@ if [ ! -f ./build.spec ]; then
     echo Sorry, can not find rpm spec file
     exit 1
 fi
+
 cp build.spec $HOME/rpmbuild/SPECS
 # here I patch the spec file to feed it with the version and the release and the date
-# sed -i -e "s/##VERSION##/${VERSION}/" -e "s/##RELEASE##/${RELEASE}/" /home/rpmbuild/rpmbuild/SPECS/${PACKNAME}.spec
-# sed -i -e "s/##DATE##/$(date +\"%a %b %d %Y\")/" /home/rpmbuild/rpmbuild/SPECS/${PACKNAME}.spec
+sed -i $HOME/rpmbuild/SPECS/build.spec \
+    -e "s/{{package}}/${PACKAGE}/g" \
+    -e "s/{{version}}/${VERSION}/g" \
+    -e "s/{{release}}/${RELEASE}/g"
+# -e "s/{{date}}/$(date +\"%a %b %d %Y\")/" \
 
 # prepare a tar.gz file with the sources and copy it  to the SOURCES directory
-tar -zcf listrepo.tar.gz ./src
-cp listrepo.tar.gz $HOME/rpmbuild/SOURCES/
+tar -zcf ${PACKAGE}.tar.gz ./src
+cp ${PACKAGE}.tar.gz $HOME/rpmbuild/SOURCES/
 
 # then execute the rpmbuild command
 cd $HOME/rpmbuild
 
-ls -al
-
 rpmbuild -ba ./SPECS/build.spec
 # copy the rpms to the artifact directory, for jenkins.
 if [[ -d /artifacts ]]; then
-    cp ./RPMS/noarch/*.rpm /artifacts/
+    cp ./RPMS/noarch/${PACKAGE}*.rpm /artifacts/
 fi
